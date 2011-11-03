d1697c0356bbea9117da8258d7637c91f574ed6f
Sky Faber <skyf@alleninstitute.org>
2011-11-02 18:10:15 -0700

    Move tests to the top level to work around macro issues

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sky/Code/fatherfinder
+# Build directory: /home/sky/Code/fatherfinder
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(sequence-tests "/home/sky/Code/fatherfinder/bin/test/Sequences/string_sequence_test")
+SUBDIRS(Sequences)
